f68ee71db67ec548121e23b6ee2f3a2c751f1164
christinewang766 <christinewang766@gmail.com>
2023-06-22 12:22:04 -0700

    figured out the dog cursor issue

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,57 +2,52 @@ import Icon from "../components/icon.svg";
 import Dog from "../components/dog.jpg";
 import ThemeStyles from "../styling/Theme.module.css";
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
-import useSound from 'use-sound';
+import { useEffect, useState } from "react";
+import useSound from "use-sound";
 import Woof from "../components/woof.mp3";
 
 // BALL CODE INSPO: https://codepen.io/reginabattle/pen/RvyjWy
-let ball = document.getElementById("ball");
+// let ballX = 0;
+// let ballY = 0;
 
-let ballX = 0;
-let ballY = 0;
-
-let speed = 0.1;
-
-const onMouseMove = (e: { pageX: number; pageY: number }) => {
-  let distX = e.pageX - ballX;
-  let distY = e.pageY - ballY;
-
-  ballX = ballX + distX * speed;
-  ballY = ballY + distY * speed;
-
-  if (ball) {
-    ball.style.left = ballX + "px";
-    ball.style.top = ballY + "px";
-  }
-};
-
-document.addEventListener("mousemove", onMouseMove);
+// let speed = 0.001;
+// USE TIME (measure time, save it, measure, difference ==> modify speed)
 
 export function Login() {
   const navigate = useNavigate();
   const [clicked, setClicked] = useState<boolean>(false);
+
   const handleMouseHover = () => {
     setClicked(false);
     stop();
-    if (ball) ball.style.visibility = "hidden";
   };
+
   const handleIconClick = () => {
     setClicked(!clicked);
-    playWoof(); 
-    if (ball) ball.style.visibility = "visible";
+    playWoof();
   };
-  
-  const [playWoof, { stop }] = useSound(Woof,
-    { volume: 1 }
-  );
 
+  const [playWoof, { stop }] = useSound(Woof, { volume: 1 });
+
+  var cursor = document.getElementById("cursor");
+  document.body.addEventListener("mousemove", function (e) {
+    if (cursor)
+      (cursor.style.left = e.clientX + "px"),
+        (cursor.style.top = e.clientY + "px");
+  });
 
   // ==========================================================================
   return (
     <div className={ThemeStyles.outerFrame} style={{ background: "#DD517E" }}>
       <div className={ThemeStyles.innerFrame} style={{ background: "#7A98ED" }}>
-        <h1 style={{fontFamily:'cursive', color:(clicked ? 'white' : '#7A98ED')}}>Want to login? You'll have to be faster than Woofie!</h1>
+        <h1
+          style={{
+            fontFamily: "cursive",
+            color: clicked ? "white" : "#7A98ED",
+          }}
+        >
+          Want to login? You'll have to be faster than Woofie!
+        </h1>
         <button style={{ background: "none", border: "none" }}>
           <img
             src={Icon}
@@ -62,22 +57,29 @@ export function Login() {
           />
         </button>
         <img
-          id="ball"
           style={{
-            position: "absolute",
-            transform: "translate(-50%,-50%)",
+            position: "fixed",
+            borderRadius: "50%",
+            transform: "translateX(-50%) translateY(-50%)",
+            pointerEvents: "none",
+            left: "-100px",
+            top: "50%",
+            backgroundColor: "transparent",
+            zIndex: "10000",
+            border: "3px solid black",
+            visibility: clicked ? "visible" : "hidden",
             height: "200px",
             width: "200px",
-            borderRadius: "50%",
-            border: "2px solid black",
-            visibility: "hidden",
+            transition: clicked ? "all 700ms ease-out" : "all 0ms ease-out",
           }}
+          id="cursor"
           src={Dog}
-        />
+        ></img>
         <button
           className={ThemeStyles.button}
           style={{ border: "5px solid #556CC9" }}
           onClick={() => {
+            stop();
             navigate("login");
           }}
         >
@@ -92,13 +94,28 @@ export function Login() {
             border: "5px solid #fff",
           }}
           onClick={() => {
+            stop();
             navigate("register");
           }}
         >
           Register
         </button>
-        <h4 style={{fontFamily:'cursive', color:(clicked ? 'white' : '#7A98ED')}}>Or...go ahead and register :D</h4>
-        <p style={{fontFamily:'cursive', color:(clicked ? 'white' : '#7A98ED')}}>(I bet mobile users are so confused right now)</p>
+        <h4
+          style={{
+            fontFamily: "cursive",
+            color: clicked ? "white" : "#7A98ED",
+          }}
+        >
+          Or...go ahead and register :D
+        </h4>
+        <p
+          style={{
+            fontFamily: "cursive",
+            color: clicked ? "white" : "#7A98ED",
+          }}
+        >
+          (I bet mobile users are so confused right now)
+        </p>
       </div>
     </div>
   );
